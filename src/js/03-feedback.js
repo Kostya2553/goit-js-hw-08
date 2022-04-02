8f2a050caabdf69b6296a76947bc0257e23eb710
kostyaget <progogyd@gmail.com>
2022-04-02 14:01:52 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,37 @@
 import throttle from 'lodash.throttle';
 
-
 const formEl = document.querySelector('.feedback-form');
 const inputEl = document.querySelector('.feedback-form input');
 const textareaEl = document.querySelector('.feedback-form textarea');
 const formData = {};
 
 
+
+
 formEl.addEventListener('input', throttle(onFormInput, 500));
 formEl.addEventListener('submit', onFormSubmit);
 
 
+setSavesData();
+function onFormSubmit(event) {
+  event.preventDefault();
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(savedData);
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function satSavesData() {
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedData.email) {
+    inputEl.value = savedData.email;
+  };
+  if (savedData.message) {
+    textareaEl.value = savedData.message;
+  };
+}
